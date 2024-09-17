2ebb2ce854f16c18e7dd85ceaa15cef2c56aae6a
Prof. John Hutton <110941287+jfhutton@users.noreply.github.com>
2024-09-17 12:56:04 -0500

    Update
    Including full starter code (students don't need to cut/paste) and saving the solution to hopefully make things simple.

--- Lab04.asm
+++ Lab04.asm
@@ -13,10 +13,24 @@ rjmp main	; set reset vector to point to the main code entry point
 
 main:       ; jump here on reset
 
-; initialize the stack (RAMEND = 0x10FF by default for the ATmega128A)
-ldi R16, HIGH(RAMEND)
-out SPH, R16
-ldi R16, low(RAMEND)
-out SPL, R16
+		; initialize the stack (RAMEND = 0x10FF by default for the ATmega128A)
+		ldi R16, HIGH(RAMEND)
+		out SPH, R16
+		ldi R16, low(RAMEND)
+		out SPL, R16
 
-; your code starts here...
+		LDI  n, 4	; load a value into n
+		PUSH n	; push it on the stack
+		CALL factN	; calculate the factorial of n
+		POP  result	; pop result off stack
+here:
+		RJMP here	; loop forever
+
+factN:
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	; Comments regarding the factN subroutine go here
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	; recursive factorial code begins here
+
+	; return from the factN subroutine
+	ret 
